aa0ded183487873c9f6511b03b3b5f0d5b16066d
Plebiaani <leevi.kalevi.hy@gmail.com>
2022-04-20 10:35:32 +0200

    updated flamesens

--- flamesens.py
+++ flamesens.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as IO
-from time import sleep
 
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
@@ -8,4 +7,13 @@ inputPins = [7]
 
 for x in inputPins:
     IO.setup(x,IO.IN)
+
+def callback(): #add the things to do, once flame is detected here
+    print("flame detected")
     
+IO.add_event_detect(inputPins[0], IO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+IO.add_event_callback(inputPins[0], callback)  # assign function to GPIO PIN, Run function on change
+ 
+# infinite loop
+while True:
+    time.sleep(1)
