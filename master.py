2db9730829a18aa95409f76b5b92666fd7b08736
Plebiaani <leevi.kalevi.hy@gmail.com>
2022-05-10 15:19:07 +0200

    updated comments in master.py

--- master.py
+++ master.py
@@ -13,7 +13,7 @@ class IOPair:   #class containg information on input / output pair
     pinOut = 0
     
     def __init__(inPin, outPin):
-        if type(inPin)is int:
+        if (type(inPin)is int)and (type(outPin)is int or type(outPin)is function):
             pinIn = inPin   #pin number to activate, NOTE output will be executed on change HIGH to LOW and on LOW to HIGH
             pinOut=outPin   #NOTE can be an intiger for a output pin OR a function to be callled
         
@@ -25,16 +25,15 @@ list = [IOPair(7,8), IOPair(9,callback)]    #list of input output pairs with exa
 #preform setup for all items in list
 for x in list:        
     IO.setup(x.pinIn,IO.IN)
+    IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300)        # let us know when the pin goes HIGH or LOW
     
-    #if pinOut is an intiger set the matching pin on GPIO to HIGH
+    #if pinOut is an intiger set the matching pin on GPIO to HIGH, NOTE is it possible to just connect thease wires?
     if type(x.pinOut) is int:   
         IO.setup(x.pinOut, IO.OUT)
-        IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300)        # let us know when the pin goes HIGH or LOW
         IO.add_event_callback(x.pinIn, IO.output(x.pinOut,IO.HIGH))  # assign function to GPIO PIN, Run function on change
     
-    #if pinOut is a function call it
-    else:                       
-        IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300) 
+    #if pinOut is a function call it, NOTE if pinOut isn't a in it's a function, see typeCheck in IOPair _init_
+    else:
         IO.add_event_callback(x.pinIn, x.pinOut)  
         
 #loop that waits for an interrupt and has an conditional exit
