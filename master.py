507bcf798dc8867f4f70259ba31fb347ef342795
Plebiaani <leevi.kalevi.hy@gmail.com>
2022-05-10 15:00:56 +0200

    added cleanup on exit

--- master.py
+++ master.py
@@ -2,6 +2,8 @@ from doctest import OutputChecker
 import RPi.GPIO as IO
 from time import sleep
 
+updateRate = 100 #how may times per second does the program check for inputs, NOTE this assumes time taken by update is negligible
+
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
 
@@ -14,32 +16,34 @@ class IOPair:   #class containg information on input / output pair
     
     def __init__(input, output):
         if type(input)is int:
-            pinIn = input
-            pinOut=output
+            pinIn = input   #pin number to activate NOTE output will be executed on change HIGH to LOW and on LOW to HIGH
+            pinOut=output   #NOTE can be an intiger for a output pin OR a function to be callled
         
         else:
             print("Error: invalid parameters in IO pair constructor")
-        
-    def Sethigh():
-        IO.output(pinOut,IO.HIGH)
 
 list = [IOPair(7,8), IOPair(9,callback)]    #list of input output pairs with examples
     
-for x in list:
+#preform setup for all items in list
+for x in list:        
     IO.setup(x.pinIn,IO.IN)
     
-    if type(x.pinOut) is int:   #if pinOut is an intiger set the matching pin on GPIO to HIGH
+    #if pinOut is an intiger set the matching pin on GPIO to HIGH
+    if type(x.pinOut) is int:   
         IO.setup(x.pinOut, IO.OUT)
-        IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-        IO.add_event_callback(x.pinIn, x.SetHigh())  # assign function to GPIO PIN, Run function on change
+        IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300)        # let us know when the pin goes HIGH or LOW
+        IO.add_event_callback(x.pinIn, IO.output(x.pinOut,IO.HIGH))  # assign function to GPIO PIN, Run function on change
     
-    else:                       #if pinOut is a function call it
+    #if pinOut is a function call it
+    else:                       
         IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300) 
         IO.add_event_callback(x.pinIn, x.pinOut)  
         
-
+#loop that waits for an interrupt and has an conditional exit
 run = True
+delay = 1/updateRate
 while run:
+            
     try:
         i = input()
     except KeyboardInterrupt:
@@ -47,4 +51,10 @@ while run:
         i = input()
         if(i=="Y" or i == "Yes" or i =="yes"):
             run = False
-    sleep(1)
+    sleep(delay)
+
+#preform cleanup on used pins when exiting
+for x in list:
+    IO.cleanup(x.pinIn)
+    if type(x.pinOut)==int:
+        IO.cleanup(x.pinOut)
