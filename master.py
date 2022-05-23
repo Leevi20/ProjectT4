f4edf0c01e36d6602650439f938d55e3b5a7ec02
Plebiaani <leevi.kalevi.hy@gmail.com>
2022-05-23 14:26:59 +0200

    finilized

--- master.py
+++ master.py
@@ -1,59 +1,82 @@
-from doctest import OutputChecker
+#pins
+#flame      2
+#fan        3
+#prox       4
+#light      5
+#light sens 6
+#servo      7
+#humidity   8
+#DC motor   9
+
 import RPi.GPIO as IO
 from time import sleep
 
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
+IO.cleanup()
+IO.setmode(IO.BCM)
+
+run = True #set to false for exiting
 
 #if your actuator needs more complicated conrolls than a high singnal on a single pin, write it into a functinon here
-
-def callback(): #example function
-    print("happening!!!!")
-
+def fan(a):
+    print("fan")
+    IO.output(3,IO.LOW)
+    sleep(3)
+    IO.output(3,IO.HIGH)
+    
+def led(a):
+    print("led")
+    IO.output(5,IO.HIGH)
+    sleep(2)
+    IO.output(5,IO.LOW)
+    
+def servo():
+    print("servo")
+    pwm = GPIO.PWM(7,50)
+    pwm.start(0)
+    IO.output(7,True)
+    pwm.ChangeDutyCycle(7)
+    sleep(1)
+    GPIO.output(7,False)
+    pwm.ChangeDutyCycle(0)
+    
+def motor():
+    print("motor")
+    sleep(1)
+    run = False
+    
 class IOPair:   #class containg information on input / output pair
     pinIn = 0
     pinOut = 0
+    func = 0
+    initial = 0
     
-    def __init__(inPin, outPin):
-        if (type(inPin)is int)and (type(outPin)is int or type(outPin)is function):
+    def __init__(self, inPin, outPin, ini, outFunc):
             pinIn = inPin   #pin number to activate, NOTE output will be executed on change HIGH to LOW and on LOW to HIGH
-            pinOut=outPin   #NOTE can be an intiger for a output pin OR a function to be callled
-        
-        else:
-            print("Error: invalid parameters in IO pair constructor")
+            pinOut=outPin   
+            func = outFunc
+            initial = ini
             
 #list of input output pairs with examples
 # to add an actuator sensor pair ass a new IOPair(input , output) to the list below
-list = [IOPair(7,8), IOPair(9,callback)]   
+list = [IOPair(2,3,IO.HIGH,fan),IOPair(4,5,IO.LOW,led),IOPair(6,7,IO.LOW,motor),IOPair(8,9,IO.LOW,motor)]
  
 #preform setup for all items in list
 for x in list:        
     IO.setup(x.pinIn,IO.IN)
     IO.add_event_detect(x.pinIn, IO.BOTH, bouncetime=300)        # let us know when the pin goes HIGH or LOW
     
-    #if pinOut is an intiger set the matching pin on GPIO to HIGH, NOTE is it possible to just connect thease wires?
-    if type(x.pinOut) is int:   
-        IO.setup(x.pinOut, IO.OUT)
-        IO.add_event_callback(x.pinIn, IO.output(x.pinOut,IO.HIGH))  # assign function to GPIO PIN, Run function on change
-    
-    #if pinOut is a function call it, NOTE if pinOut isn't an int it's a function, see typeCheck in IOPair :_init__
-    else:
-        IO.add_event_callback(x.pinIn, x.pinOut)  
+    IO.setup(x.pinOut, IO.OUT, x.initial)
+    IO.add_event_callback(x.pinIn, x.func)  # assign function to GPIO PIN, Run function on change 
         
 #loop that waits for an interrupt and has an conditional exit
-run = True
 while run:
-    try:
-        i = input()
-    except KeyboardInterrupt:
-        print("want to exit? Y/N")
-        i = input()
-        if(i=="Y" or i == "Yes" or i =="yes"):
-            run = False
     sleep(.1)
 
 #preform cleanup on used pins when exiting
 for x in list:
     IO.cleanup(x.pinIn)
-    if type(x.pinOut)==int:
-        IO.cleanup(x.pinOut)
+    IO.cleanup(x.pinOut)
+
+print("exiting")
