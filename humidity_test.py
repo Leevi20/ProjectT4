27376a73c60fbcf6da553f282eb69aee509e7811
Plebiaani <leevi.kalevi.hy@gmail.com>
2022-06-02 13:25:00 +0200

    updated pin number in humidity test

--- humidity_test.py
+++ humidity_test.py
@@ -10,8 +10,8 @@ import datetime
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
-# read data using pin 14
-instance = dht11.DHT11(pin=14)
+# read data using pin 23
+instance = dht11.DHT11(pin=23)
 
 try:
 	while True:
